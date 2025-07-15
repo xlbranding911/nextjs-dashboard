2ced4543ca934d70f0d27cde0ff745fbe2f5fb85
xlbranding911 <xl.branding911@gmail.com>
2025-07-15 17:31:24 +0800

    Update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,11 @@
 'use server';
 
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export async function authenticate(
   prevState: string | undefined,
--- auth.ts
+++ auth.ts
@@ -20,19 +20,20 @@ async function getUser(email: string): Promise<User | undefined> {
  
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [    
-      Credentials({
+  providers: [
+    Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
           
         if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
-            if (!user) return null;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
-            if (passwordsMatch) return user;
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          
+          if (passwordsMatch) return user;
         }
         console.log('Invalid credentials');
         return null;
